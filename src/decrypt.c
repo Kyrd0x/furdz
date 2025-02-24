263bb7f17c65bc9ddab6f602c8d6870953667bba
Kyrd0x <remi@couzi.fr>
2025-02-24 11:01:52 +0000

    xor rdy

--- src/decrypt.c
+++ src/decrypt.c
@@ -2,21 +2,45 @@
 
 
 
+
+// CQFD
 void XOR(unsigned char *data, size_t len, BYTE key) {
     for (size_t i = 0; i < len; i++) {
         data[i] ^= key;
     }
 }
 
-void DICT_decrypt(unsigned char *data, size_t len, BYTE key) {
-    for (size_t i = 0; i < len; i++) {
-        data[i] = data[i] ^ key;
-    }
-}
+
+// // Word decryption
+// unsigned char* DICT_decrypt(const char *message) {
+//     int assoc_size = sizeof(tableau) / sizeof(tableau[0]); // Calculer la taille du tableau d'associations
+//     static unsigned char payload[TAILLE];  // Tableau pour stocker les octets associés
+//     int result_index = 0;
+
+//     // Dupliquer la chaîne pour éviter de modifier l'originale
+//     char buffer[TAILLE * 8]; 
+//     strcpy(buffer, message);  // Copie le message dans le buffer
+
+//     // Découper la chaîne en mots
+//     char *token = strtok(buffer, " ");
+//     while (token != NULL) {
+//         for (int i = 0; i < assoc_size; i++) {
+//             // Si le mot correspond à un mot dans le tableau, ajouter l'octet correspondant à la payload
+//             if (strcmp(token, tableau[i].mot) == 0) {
+//                 payload[result_index++] = tableau[i].octet;
+//                 break;
+//             }
+//         }
+//         token = strtok(NULL, " "); // Passer au mot suivant
+//     }
+
+//     // Retourner l'adresse de la payload
+//     return payload;
+// }
 
 //todo
-void AES_decrypt(unsigned char *data, size_t len, BYTE key) {
-    for (size_t i = 0; i < len; i++) {
-        data[i] = data[i] ^ key;
-    }
-}
+// void AES_decrypt(unsigned char *data, size_t len, BYTE key) {
+//     for (size_t i = 0; i < len; i++) {
+//         data[i] = data[i] ^ key;
+//     }
+// }
