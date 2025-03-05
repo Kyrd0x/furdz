c58c9cf7821f44951681031401cf1261eeda2798
Couscouz <rem.couzi@gmail.com>
2025-03-05 20:24:31 +0100

    rar added

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Install necessary packages for compilation
-if [[ "$(uname -s)" == "Linux" ]]; then
-    echo "Installing dependencies for compilation..."
-    sudo apt update && sudo apt install -y mingw-w64 gcc make
+if grep -qE '^ID=(ubuntu|debian|kali)' /etc/os-release; then
+    echo "Installing dependencies for Debian-based systems..."
+    sudo apt-get update && sudo apt-get install -y mingw-w64 gcc make rar
+elif grep -qE '^ID=arch' /etc/os-release; then
+    echo "Installing dependencies for Arch Linux..."
+    sudo pacman -Sy --noconfirm mingw-w64-gcc gcc make rar
 else
-    echo "This script is only for Linux"
+    echo "This script is only for Debian-based or Arch Linux."
     exit 1
 fi
 
