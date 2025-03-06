ab8e37ca8ea04561fa918a47f8d94681e1a8a60a
Kyrd0x <remi@couzi.fr>
2025-03-06 15:27:28 +0100

    sfx

--- pdf/create.sh
+++ pdf/create.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OUTPUT_FILE=archive_sfx.exe
+rm pdf/output/$OUTPUT_FILE
 cp bin/*.exe pdf/files/
-rar a -sfx -ipdf.ico -zpdf/config.sfx pdf/output/archive_sfx.exe pdf/files/*
+rar a -sfx -ipdf/pdf.ico -zpdf/config.sfx pdf/output/$OUTPUT_FILE pdf/files/
 echo "SFX archive generated"
